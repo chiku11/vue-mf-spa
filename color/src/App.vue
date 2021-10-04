75c20bd037f56e2f5cd242fa277a7a76fb7ac617
Srikant Vavilapalli <srikant.rsm@gmail.com>
2021-10-03 22:59:40 -0500

    bootstrap integration

--- color/src/App.vue
+++ color/src/App.vue
@@ -1,14 +1,23 @@
 <template>
- <div id="color">
-     <h1>Select a color:</h1>
-     <Header />
- </div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+          Pick a color
+      </div>
+      <div class="col">
+        <Header />
+      </div>
+      <div class="col">
+        Still in development
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import Header from 'Design/Header';
+import Header from "Design/Header";
 export default {
- components: {
-     Header,
- },   
-}
+  components: {
+    Header,
+  },
+};
 </script>
--- color/src/bootloader.js
+++ color/src/bootloader.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 import "./index.css";
 
 import App from "./App.vue";
 
+// Make BootstrapVue available throughout your project
+// Optionally install the BootstrapVue icon components plugin
 createApp(App).mount("#app");
